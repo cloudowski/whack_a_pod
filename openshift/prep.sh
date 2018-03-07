7326c1a88f58dd84b4269a3ea0967c5d31c34dc0
Tomasz Cholewa <tomasz@cloudowski.com>
2018-03-07 18:00:36 +0100

    Openshift scripts

--- openshift/prep.sh
+++ openshift/prep.sh
@@ -1,3 +1,5 @@
-oc create serviceaccount game
-oc policy add-role-to-user view -z game
-oc adm policy add-scc-to-user anyuid -z game
+oc create serviceaccount wap-game
+oc create serviceaccount wap-admin
+oc policy add-role-to-user view -z wap-game
+oc policy add-role-to-user edit -z wap-admin
+oc adm policy add-scc-to-user anyuid -z wap-game
---
+++ openshift/run.sh
@@ -0,0 +1,18 @@
+oc new-app --docker-image=cloudowski/whackapod-game -l stack=whackapod
+oc new-app --docker-image=cloudowski/whackapod-admin -l stack=whackapod --env="APIIMAGE=cloudowski/whackapod-api"
+oc new-app --docker-image=cloudowski/whackapod-api -l stack=whackapod
+
+oc patch dc/whackapod-admin --patch '{"spec":{"template":{"spec":{"serviceAccountName": "wap-admin"}}}}'
+oc patch dc/whackapod-game --patch '{"spec":{"template":{"spec":{"serviceAccountName": "wap-game"}}}}'
+
+
+oc expose dc/whackapod-admin --port=8080
+oc expose dc/whackapod-game --port=8080
+oc expose dc/whackapod-api --port=8080
+oc expose svc/whackapod-game --path=/ --port=8080
+
+sleep 5
+GAMEHOST=$(oc get route whackapod-game -o=jsonpath='{.status.ingress[0].host}')
+
+oc expose svc/whackapod-admin --hostname=$GAMEHOST --path=/admin
+oc expose svc/whackapod-api --hostname=$GAMEHOST --path=/api
